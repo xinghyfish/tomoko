14a82341ded44f65802e8c90cbc7f2045817b4d8
xinghyfish <xinghy.fish@qq.com>
2025-03-25 21:29:48 +0800

    feat: 添加了饮水器龙头的识别方法，

--- tomoko.py
+++ tomoko.py
@@ -7,9 +7,9 @@ from numpy.ma.core import arctan
 
 from entity import GraspInfo
 from entity.entity import *
-from robot_arm.command import EndPoseMoveCommand, BottomTurnCommand, WristRollCommand, GripperCloseCommand, \
-    SetZeroCommand, LiftMoveCommand, GripperOpenCommand, GraspCommand
 from jarvis import Jarvis
+from robot_arm.command import EndPoseMoveCommand, BottomTurnCommand, WristRollCommand, GraspCommand, \
+    JointControlCommand, LiftMoveCommand
 from robot_arm.pipeline import Pipeline
 from vlm.image_utils import center_of_mask
 from vlm.lang_sam_demo import show_masks_on_image
@@ -43,7 +43,7 @@ class Tomoko:
         prompt = "yellow circle in teapot"
         obj_lst, color_image, depth_image, depth_intrin, depth_frame = self.jarvis.detect(prompt)
         assert obj_lst, len(obj_lst) == 1
-        xe, ye, dis, mask1 = obj_lst[0]
+        xe, ye, dis, mask1, _ = obj_lst[0]
 
         # segment the part of handle from the whole
         teapot_depth = depth_image * mask1
@@ -51,7 +51,7 @@ class Tomoko:
         # find the index of the nearest part
         min_index = np.unravel_index(np.argmin(replaced_teapot_depth), replaced_teapot_depth.shape)
         min_depth = teapot_depth[min_index]
-        thickness = 20
+        thickness = 10
         # so this is the mask of handle
         wooden_mask = np.where(abs(teapot_depth - min_depth) <= thickness, 1, 0)[np.newaxis, :]
         show_masks_on_image(color_image, wooden_mask)
@@ -71,20 +71,48 @@ class Tomoko:
         prompt = f"{color} can"
         objects_position, _, _, depth_intrin, _ = self.jarvis.detect(prompt)
         assert objects_position, len(objects_position) == 1
-        x, y, distance, mask = objects_position[0]
+        x, y, distance, mask, _ = objects_position[0]
         entity = TeaCan()
         end_pose, target_index = self.jarvis.pixel_to_3d(x, y, distance, depth_intrin, entity)
         self.grasp_info[prompt] = GraspInfo(target_index, end_pose)
         logging.info(f"{prompt} position: {target_index}")
         return target_index
 
+    def detect_water_dispenser(self):
+        pos = [-35, 10, -10, 0, 10, 0]
+        prompt = "small black rectangle on the bottom"
+
+        self.jarvis.arm_controller.joint_control(pos)
+        objects_position, _, _, depth_intrin, _ = self.jarvis.detect(prompt)
+        boxes = [_[-1] for _ in objects_position]
+        left_idx = 0 if boxes[0][0] < boxes[1][0] else 1
+        right_idx = 1 - left_idx
+        faucets = {'left': left_idx, 'right': right_idx}
+
+        entity = Faucet()
+
+        for key in faucets.keys():
+            x, y, distance, mask, _ = objects_position[faucets[key]]
+            end_pose, target_index = self.jarvis.pixel_to_3d(x, y, distance, depth_intrin, entity)
+            X, Y, Z, RX, RY, RZ = end_pose
+            back_factor = Teapot().inner_radius * 2 / math.sqrt(X ** 2 + Y ** 2)
+            X *= (1 - back_factor)
+            Y *= (1 - back_factor)
+            end_pose = [X, Y, Z,RX, RY, RZ]
+            self.grasp_info[faucets[key]] = GraspInfo(target_index, end_pose)
+
+
+
+
     def add_tea_to_teapot(self, color):
-        self.detect_teapot()
-        tea_can_detectable_joints = [35, 10, -20, 0, 30, 0]
-        self.jarvis.arm_controller.joint_control(tea_can_detectable_joints)
-        time.sleep(1)
-        self.detect_can(color)
-        self.jarvis.arm_controller.set_grip_degree(70)
+        """
+        Sub-task 1: Add tea from tea can to teapot.
+        :param color: color of the tea can
+        :return:
+        """
+        init_joints = self.jarvis.arm_controller.joint_state()
+        via_joints = [45, 10, -20, 0, 30, 0]
+        self.jarvis.arm_controller.joint_control(via_joints)
         grasp_target = f"{color} can"
         self.current_grasp_object = grasp_target
         end_pose_can = self.grasp_info[grasp_target].end_pose
@@ -136,18 +164,37 @@ class Tomoko:
         # this is a time-consuming operation
         time.sleep(2)
         # go back
-        drop_tea_pipeline.add_command_reverse(SetZeroCommand(self))
         drop_tea_pipeline.undo()
+
+        self.jarvis.arm_controller.lift(10)
+        self.jarvis.arm_controller.joint_control(via_joints)
+        self.jarvis.arm_controller.joint_control(init_joints)
+
+
+    def scan_desk(self):
+        teapot_detectable_joints = [0, 0, 0, 0, 10, 0]
+        self.jarvis.arm_controller.joint_control(teapot_detectable_joints)
+        self.detect_teapot()
+        tea_can_detectable_joints = [40, 10, -20, 0, 30, 0]
+        time.sleep(3)
+        # suppose color
+        colors = ["silver", "red", "yellow"]
+        for color in colors:
+            self.jarvis.arm_controller.joint_control(tea_can_detectable_joints)
+            time.sleep(1)
+            self.detect_can(color)
+            print(color)
+            print(self.jarvis.arm_controller.joint_state())
+        self.jarvis.arm_controller.joint_control(teapot_detectable_joints)
 
 
 def pipeline():
     tomoko = Tomoko()
-    teapot_detectable_joints = [0, 0, 0, 0, 10, 0]
-    tomoko.jarvis.arm_controller.joint_control(teapot_detectable_joints)
-    time.sleep(2)
-    tomoko.add_tea_to_teapot("red")
+    tomoko.jarvis.arm_controller.end_pose_control(tomoko.jarvis.arm_controller.init_end_pose)
+    # tomoko.scan_desk()
+    # tomoko.add_tea_to_teapot("red")
 
 
 if __name__ == '__main__':
-    time.sleep(5)
+    # time.sleep(5)
     pipeline()
