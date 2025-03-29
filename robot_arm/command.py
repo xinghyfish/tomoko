9cbc288176347fbdb379d9c4d2a157c94f614ba8
xinghyfish <xinghy.fish@qq.com>
2025-03-29 14:17:20 +0800

    update: 添加了更多时间控制方式；添加计时器命令；在GraspCommand的undo过程中添加了碰撞避免的中间状态

--- robot_arm/command.py
+++ robot_arm/command.py
@@ -2,8 +2,6 @@ import time
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
-from sympy import andre
-
 
 class Command(ABC):
     def __init__(self, tomoko):
@@ -27,10 +25,14 @@ class BottomTurnCommand(Command):
         self.angle = args[0]
 
     def execute(self):
-        return self.arm_controller.bottom_turn(self.angle)
+        flag = self.arm_controller.bottom_turn(self.angle)
+        time.sleep(2)
+        return flag
 
     def undo(self):
-        return self.arm_controller.bottom_turn(-self.angle)
+        flag = self.arm_controller.bottom_turn(-self.angle)
+        time.sleep(2)
+        return flag
 
     def __str__(self):
         return f"Command(Bottom Turn) - {self.angle} degree"
@@ -71,22 +73,29 @@ class GraspCommand(Command):
         super().__init__(tomoko)
         self.target = target
         self.end_pose = end_pose
+        self.last_joints = None
 
     def execute(self):
+        self.last_joints = self.arm_controller.joint_state()
         gripper_open = self.arm_controller.set_grip_degree(70)
         time.sleep(1)
         end_pose_control = self.arm_controller.end_pose_control(self.end_pose)
         time.sleep(1)
         gripper_close = self.arm_controller.set_grip_degree(0)
+        time.sleep(1)
         return all([gripper_open, end_pose_control, gripper_close])
 
     def undo(self):
-        gripper_close = self.arm_controller.set_grip_degree(0)
-        time.sleep(1)
-        end_pose_control = self.arm_controller.end_pose_control(self.end_pose, move_mode=0x2)
-        time.sleep(1)
         gripper_open = self.arm_controller.set_grip_degree(70)
-        return all([gripper_close, end_pose_control, gripper_open])
+        time.sleep(1)
+        lift = self.arm_controller.lift(20)
+        time.sleep(1)
+        via_end_pose = [256.152, -24.306, 190.406, -80.134, 88.019, -85.915]
+        via_flag = self.arm_controller.end_pose_control(via_end_pose, 0x00, 50)
+        time.sleep(2)
+        init_joints_control = self.arm_controller.joint_control(self.last_joints)
+        time.sleep(2)
+        return all([gripper_open, lift, via_flag, init_joints_control])
 
     def __str__(self):
         return f"Command(Grasp) - {self.target}"
@@ -99,29 +108,51 @@ class EndPoseMoveCommand(Command):
         self.end_pose = args[0]
         self.move_mode = 0x2 if args[1] == "linear" else 0x0
         self.move_speed_rate = args[2]
+        self.last_end_pose = None
 
     def execute(self):
+        self.last_end_pose = self.arm_controller.end_pose_state()
         return self.arm_controller.end_pose_control(self.end_pose, self.move_mode, self.move_speed_rate)
 
     def undo(self):
-        return self.execute()
+        return self.arm_controller.end_pose_control(self.last_end_pose, self.move_mode, self.move_speed_rate)
 
     def __str__(self):
-        return f"Command(End Pose) - [{self.end_pose}]"
+        return f"Command(End Pose) - {[round(_, 3) for _ in self.end_pose]}"
 
 
 class JointControlCommand(Command):
     def __init__(self, tomoko, *args):
         super().__init__(tomoko)
+        self.last_state = None
         assert len(args) == 2
         self.joints = args[0]
         self.move_speed_rate = args[1]
 
     def execute(self):
+        self.last_state = self.arm_controller.joint_state()
         return self.arm_controller.joint_control(self.joints, move_speed_rate=self.move_speed_rate)
 
     def undo(self):
-        return self.execute()
+        return self.arm_controller.joint_control(self.last_state, move_speed_rate=self.move_speed_rate)
 
     def __str__(self):
-        return f"Command(Joints) - [{self.joints}]"
+        return f"Command(Joints) - {[round(_, 3) for _ in self.joints]}"
+
+
+class TimerCommand(Command):
+    def __init__(self, tomoko, *args):
+        """time elapse in second"""
+        super().__init__(tomoko)
+        assert len(args) == 1
+        self.seconds = args[0]
+
+    def execute(self):
+        time.sleep(self.seconds)
+        return True
+
+    def undo(self):
+        return True
+
+    def __repr__(self):
+        return f"Command(Timer) - elapse {self.seconds} seconds"
