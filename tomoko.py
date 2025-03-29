4a98f0af3b43e61a5f6888a121d9cd3c3694c5d4
xinghyfish <xinghy.fish@qq.com>
2025-03-29 14:19:34 +0800

    feat: 茶壶取水逻辑

--- tomoko.py
+++ tomoko.py
@@ -9,7 +9,7 @@ from entity import GraspInfo
 from entity.entity import *
 from jarvis import Jarvis
 from robot_arm.command import EndPoseMoveCommand, BottomTurnCommand, WristRollCommand, GraspCommand, \
-    JointControlCommand, LiftMoveCommand
+    JointControlCommand, LiftMoveCommand, TimerCommand
 from robot_arm.pipeline import Pipeline
 from vlm.image_utils import center_of_mask
 from vlm.lang_sam_demo import show_masks_on_image
@@ -51,7 +51,7 @@ class Tomoko:
         # find the index of the nearest part
         min_index = np.unravel_index(np.argmin(replaced_teapot_depth), replaced_teapot_depth.shape)
         min_depth = teapot_depth[min_index]
-        thickness = 10
+        thickness = 20
         # so this is the mask of handle
         wooden_mask = np.where(abs(teapot_depth - min_depth) <= thickness, 1, 0)[np.newaxis, :]
         show_masks_on_image(color_image, wooden_mask)
@@ -79,29 +79,74 @@ class Tomoko:
         return target_index
 
     def detect_water_dispenser(self):
-        pos = [-35, 10, -10, 0, 10, 0]
         prompt = "small black rectangle on the bottom"
 
-        self.jarvis.arm_controller.joint_control(pos)
         objects_position, _, _, depth_intrin, _ = self.jarvis.detect(prompt)
         boxes = [_[-1] for _ in objects_position]
+        print(boxes)
         left_idx = 0 if boxes[0][0] < boxes[1][0] else 1
         right_idx = 1 - left_idx
         faucets = {'left': left_idx, 'right': right_idx}
-
         entity = Faucet()
 
         for key in faucets.keys():
             x, y, distance, mask, _ = objects_position[faucets[key]]
             end_pose, target_index = self.jarvis.pixel_to_3d(x, y, distance, depth_intrin, entity)
-            X, Y, Z, RX, RY, RZ = end_pose
-            back_factor = Teapot().inner_radius * 2 / math.sqrt(X ** 2 + Y ** 2)
-            X *= (1 - back_factor)
-            Y *= (1 - back_factor)
-            end_pose = [X, Y, Z,RX, RY, RZ]
-            self.grasp_info[faucets[key]] = GraspInfo(target_index, end_pose)
+            self.grasp_info[key] = GraspInfo(target_index, end_pose)
+
+    def add_water(self, side):
+        """
+        Grasp teapot, push faucet and get water. Specify direction to get water
+        with different temperature.
+        :param side: left = # Hot, right = # Cold
+        """
+        # 0. satisfy the prerequisites
+        assert self.grasp_info.get('teapot') is not None    # detect_teapot is done
+        assert self.grasp_info.get('left') is not None      # detect_faucet is done
+        assert self.grasp_info.get('right') is not None
+
+        # 1. grasp teapot
+        add_water_pipeline = Pipeline('add water')
+        joints = [0, 10, -20, 0, 30, -5]
+        add_water_pipeline.add_command(JointControlCommand(self, joints, 20))
+        teapot_grasp_end_pose = self.grasp_info['teapot'].end_pose
+        add_water_pipeline.add_command(GraspCommand(self, 'teapot', teapot_grasp_end_pose))
+
+        # 2. via moves states
+        end_pose_faucet = self.grasp_info[side].end_pose
+        X_faucet, Y_faucet, Z_faucet, RX_faucet, RY_faucet, RZ_faucet = end_pose_faucet
+        X_teapot, Y_teapot, Z_teapot, RX_teapot, RY_teapot, RZ_teapot = teapot_grasp_end_pose
+        # via_end_pose = [X_teapot, Y_teapot, Z_teapot , RX_teapot, RY_teapot, RZ_teapot]
+
+        # add_water_pipeline.add_command(EndPoseMoveCommand(self, via_end_pose, "linear", 20))
+        add_water_pipeline.add_command(LiftMoveCommand(self, 10))
+
+        # X_back, Y_back, Z_back = X_teapot * 0.5, Y_teapot * 0.5, Z_teapot
+        # back_end_pose = [X_back, Y_back, Z_back, RX_teapot, RY_teapot, RZ_teapot]
+        theta_back = math.atan(Y_teapot / X_teapot)
+        theta_faucet = math.atan(Y_faucet / X_faucet)
 
 
+        delta_theta = math.degrees(theta_faucet - theta_back)
+        add_water_pipeline.add_command(BottomTurnCommand(self, delta_theta))
+
+        # get water
+        delta_distance = 118   # push forward distance to get water
+        delta_teapot = -(Teapot().radius * 2) - 10
+        distance_push_faucet = delta_teapot + delta_distance
+        X_push_faucet, Y_push_faucet, Z_push_faucet = (
+            X_faucet + distance_push_faucet * math.cos(theta_faucet),
+            Y_faucet + distance_push_faucet * math.sin(theta_faucet),
+            Z_faucet - 10
+        )
+        end_pose_push_faucet = [X_push_faucet, Y_push_faucet, Z_push_faucet, RX_faucet, RY_faucet, RZ_faucet]
+
+        add_water_pipeline.add_command(EndPoseMoveCommand(self, end_pose_push_faucet, "linear", 20))
+        wait_add_water_elapse_second = 2   # waiting...
+        add_water_pipeline.add_command(TimerCommand(self, wait_add_water_elapse_second))
+
+        if add_water_pipeline.run() and add_water_pipeline.undo():
+            return True
 
 
     def add_tea_to_teapot(self, color):
@@ -170,9 +215,8 @@ class Tomoko:
         self.jarvis.arm_controller.joint_control(via_joints)
         self.jarvis.arm_controller.joint_control(init_joints)
 
-
     def scan_desk(self):
-        teapot_detectable_joints = [0, 0, 0, 0, 10, 0]
+        teapot_detectable_joints = [0, 10, -20, 0, 30, 0]
         self.jarvis.arm_controller.joint_control(teapot_detectable_joints)
         self.detect_teapot()
         tea_can_detectable_joints = [40, 10, -20, 0, 30, 0]
@@ -190,7 +234,25 @@ class Tomoko:
 
 def pipeline():
     tomoko = Tomoko()
-    tomoko.jarvis.arm_controller.end_pose_control(tomoko.jarvis.arm_controller.init_end_pose)
+    pos = [0, 10, -10, 0, 30, -5]
+    tomoko.jarvis.arm_controller.joint_control(pos)
+    time.sleep(2)
+    tomoko.detect_teapot()
+
+    pos = [-40, 10, -10, 0, 10, -5]
+    tomoko.jarvis.arm_controller.joint_control(pos)
+    time.sleep(2)
+    tomoko.detect_water_dispenser()
+    time.sleep(1)
+
+    pos = [0, 10, -10, 0, 30, -5]
+    tomoko.jarvis.arm_controller.joint_control(pos)
+
+    for info in tomoko.grasp_info.items():
+        print(info)
+
+    # tomoko.jarvis.arm_controller.joint_control([0] * 6)
+    tomoko.add_water('left')
     # tomoko.scan_desk()
     # tomoko.add_tea_to_teapot("red")
 
