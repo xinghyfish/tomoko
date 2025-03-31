6680125f7b663a84c159079cf47721db77a1181c
xinghyfish <xinghy.fish@qq.com>
2025-03-31 21:11:57 +0800

    fix: 修正部分茶具规格

--- environment/entity.py
+++ environment/entity.py
@@ -8,16 +8,17 @@ class Entity:
 
 class TeaCup(Entity):
     def __init__(self):
-        radius, polar_angle = 32, 90
+        radius, polar_angle = 27.5, 90
         super().__init__(radius, polar_angle)
-        self.height = 48
+        self.height = 43.3
 
 
 class TeaCan(Entity):
     def __init__(self):
-        radius, polar_angle = 35, 90
+        radius, polar_angle = 22, 90
         super().__init__(radius, polar_angle)
-        self.height = 24
+        self.height = 53.6
+        self.cover = 10
 
 
 class Teapot(Entity):
@@ -25,6 +26,8 @@ class Teapot(Entity):
         radius, polar_angle = 85, 110
         super().__init__(radius, polar_angle)
         self.inner_radius = 45
+        self.height = 64.1
+        self.cover = 15.5
 
 
 class Faucet(Entity):
